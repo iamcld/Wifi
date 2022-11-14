fd5c0c706a25acc842cba8ade30dbe646add6189
chenlida <498487810@qq.com>
2022-11-14 14:40:16 +0800

    Wifi连接Demo

---
+++ app/src/main/java/com/hello/wifi/MainActivity.java
@@ -0,0 +1,43 @@
+package com.hello.wifi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.hello.wifi.tookit.IWifiConnectListener;
+import com.hello.wifi.tookit.WifiManagerProxy;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        test();
+    }
+
+
+    private void test() {
+        WifiManagerProxy.get().init(getApplication());
+        WifiManagerProxy.get().connect("HUAWEI-phoneGroup_5G", "mobile123", new IWifiConnectListener() {
+            @Override
+            public void onConnectStart() {
+                Log.i("TAG", "onConnectStart: ");
+            }
+
+            @Override
+            public void onConnectSuccess() {
+                Log.i("TAG", "onConnectSuccess: ");
+            }
+
+            @Override
+            public void onConnectFail(String errorMsg) {
+                Log.i("TAG", "onConnectFail: " + errorMsg);
+            }
+        });
+    }
+
+}
